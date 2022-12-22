ab17180c6ba6ec2b538598381dbc1feec1abd3b2
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-12-22 18:09:27 +0100

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://amunds1.github.io",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
