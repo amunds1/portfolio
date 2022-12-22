172b9a5db02737c59e92b1157be51da8d8b6a159
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-12-22 18:06:18 +0100

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://amunds1.github.io",
-  base: "/portfolio",
-  integrations: [image()]
 });
