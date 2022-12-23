fa8592785a47dab7cfa5e4c7bed98bee33ad0965
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-12-23 12:19:18 +0100

    Cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-// import node from "@astrojs/node";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://amunds1.github.io",
 });
